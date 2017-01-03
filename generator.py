efde8db855d8f750553412a72612fdf2b44f29cf
Victor Nouvellet <victor.nouvellet@gmail.com>
2017-01-03 11:56:49 +0100

    Add better URLs management, add request managing by packages

--- generator.py
+++ generator.py
@@ -3,21 +3,26 @@ import grequests
 #import requests
 import json
 
-number_of_requests = 1
+number_of_requests_package = 100
+number_of_concurrent_requests = 100
+local = True
 
 # The URL where to post data
-# URL = 'https://yagan.herokuapp.com/newGeo'
-URL = 'http://127.0.0.1:3000/newGeo'
+production_url = 'https://yagan.herokuapp.com/newGeo'
+local_url = 'http://127.0.0.1:3000/newGeo'
+URL = local_url if local else production_url
 
 # Data to send (here in data.json file)
 with open("./data.json") as json_data:
     data = json.load(json_data)
 
-# Easy asynchronous HTTP Requests
-r = (grequests.post(u, data = data) for u in [URL])
-
-friends = grequests.map(r)
-print(friends[0].text)
+i = 0
+r = []
+while i < number_of_requests_package:
+    # Easy asynchronous HTTP Requests
+    r = (grequests.post(URL, data = data) for i in range(0, number_of_concurrent_requests))
+    response = grequests.map(r)
+    i += 1
 
 # Or using requests
 # r = requests.post(URL, data)
