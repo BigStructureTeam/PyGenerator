39d34a04fe12ddacf417002ae4af12ff9ad27201
Kevin Pitolin <kevin@Kevins-MacBook-Pro.local>
2017-01-16 01:32:46 +0100

    Corrected generator sleep and time computation

--- generator.py
+++ generator.py
@@ -5,27 +5,33 @@ import random
 
 
 generator_interval = 1
-number_of_concurrent_requests = 100
+number_of_concurrent_requests = 2500
 #The kafka producer
 producer = KafkaProducer(bootstrap_servers = ['localhost:9092'], value_serializer=lambda v: json.dumps(v).encode('utf-8'), batch_size=0)
 
-i = 0
+# i = 0
 while True:
-    for _ in range(number_of_concurrent_requests):
-        start = time.time()
+
+    for i in range(number_of_concurrent_requests):
+
+    	if (i==0):
+    		start = time.time()
+
         data = json.dumps({
             "data": {
                 "latitude": random.uniform(45.730, 45.84), "longitude": random.uniform(4.680, 4.830), "timestamp": time.time()
-
             },
             "metadata": {
-                "msisdn": "33684525322",
+                "msisdn": str(random.randint(33600000000,33799999999)),
                 "radius": 150
             }
         })
-        producer.send('geoloc', data)
+        producer.send('geodata', data)
     end = time.time()
-    time.sleep(generator_interval - end + start)
+    print("time used:")
+    print(end - start)
+    if generator_interval - end + start > 0:
+    	time.sleep(generator_interval - end + start)
 producer.close(0)
 # Or using requests
 # r = requests.post(URL, data)
